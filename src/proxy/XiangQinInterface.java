a0fd1f28107c761962d2b00e6350f35dae75fedb
jxx <jxx@erongdu.com>
2015-10-08 10:48:26 +0800

    jdk动态代理

---
+++ src/proxy/XiangQinInterface.java
@@ -0,0 +1,14 @@
+package proxy;
+
+public interface XiangQinInterface {
+	
+	/**
+	 * 
+	 * @Title: xiangQin 
+	 * @Description: 相亲方法
+	 * @param  
+	 * @return void
+	 * @throws
+	 */
+	public void xiangQin();  
+}
---
+++ src/proxy/ZhangSanXiangQinInterfaceImpl.java
@@ -0,0 +1,10 @@
+package proxy;
+
+public class ZhangSanXiangQinInterfaceImpl implements XiangQinInterface {
+
+	@Override
+	public void xiangQin() {
+		System.out.println("张三去相亲，娶个漂亮老婆。");   
+	}
+
+}
